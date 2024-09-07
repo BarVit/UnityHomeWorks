0fa897b63facbd6bc6b5fc92cac30c7e66515806
Vitaliy <bavi.mail@ya.ru>
2024-09-07 23:40:34 +0300

    Counter

--- Counter/Assets/Scripts/Counter.cs
+++ Counter/Assets/Scripts/Counter.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using System.Collections;
 
 public class Counter : MonoBehaviour
 {
@@ -9,17 +10,33 @@ public class Counter : MonoBehaviour
 
     private bool _isCounting = false;
 
+    private IEnumerator Count()
+    {
+        while (true)
+        {
+            if (_isCounting)
+            {
+                _counter += Time.deltaTime;
+                DisplayCounter(_counter);
+            }
+
+            yield return null;
+        }
+    }
+
+    private void DisplayCounter(float counter)
+    {
+        _text.text = counter.ToString();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Count());
+    }
+
     private void Update()
     {
-        if(Input.GetMouseButton(0))
-        {
+        if (Input.GetMouseButton(0))
             _isCounting = !_isCounting;
-        }
-
-        if (_isCounting)
-        {
-            _counter += Time.deltaTime;
-            _text.text = _counter.ToString();
-        }
     }
 }
