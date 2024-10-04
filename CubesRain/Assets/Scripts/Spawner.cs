803a836729a4d9d5ca816fd6569baf5a077c3e55
Vitaliy <bavi.mail@ya.ru>
2024-10-04 16:00:57 +0300

    Cubes Rain

--- CubesRain/Assets/Scripts/Spawner.cs
+++ CubesRain/Assets/Scripts/Spawner.cs
@@ -7,6 +7,9 @@ public class Spawner : MonoBehaviour
 
     private int _poolCapacity = 10;
     private int _poolMaxSize = 100;
+
+    private float _spawnRate = 0.2f;
+
     private ObjectPool<GameObject> _pool;
 
     private void Awake()
@@ -21,8 +24,33 @@ public class Spawner : MonoBehaviour
             maxSize: _poolMaxSize);
     }
 
-    private void ActionOnGet(GameObject gameObject)
+    private void ActionOnGet(GameObject obj)
     {
-
+        obj.transform.position = SpawnPosition();
+        obj.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        obj.SetActive(true);
     }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(GetCube), 0, _spawnRate);
+    }
+
+    private void GetCube()
+    {
+        _pool.Get();
+    }
+
+    private Vector3 SpawnPosition()
+    {
+        int topCorner = 10;
+        int bottomCorner = -10;
+        int leftCorner = -10;
+        int rightCorner = 10;
+        int minHegith = 25;
+        int maxHeight = 35;
+
+        return new Vector3(Random.Range(leftCorner, rightCorner),
+            Random.Range(minHegith, maxHeight), Random.Range(bottomCorner,topCorner));
+    }    
 }
