9f680b149a89952659a0c3792f2ed3ed65de3834
Vitaliy <bavi.mail@ya.ru>
2024-10-05 00:02:13 +0300

    Fix names

--- CubesRain/Assets/Scripts/CubeColor.cs
+++ CubesRain/Assets/Scripts/CubeColor.cs
@@ -1,38 +1,17 @@
 using UnityEngine;
-using System;
 
-[RequireComponent(typeof(CubeColor), typeof(Rigidbody))]
-public class Cube : MonoBehaviour
+[RequireComponent(typeof(Renderer))]
+public class CubeColor : MonoBehaviour
 {
-    private CubeColor _color;
-    private Rigidbody _rigidbody;
-
-    private bool _isFirstCollision = true;
-
-    public static event Action<Cube> Collided;
+    private Material _material;
 
     private void Awake()
     {
-        _color = GetComponent<CubeColor>();
-        _rigidbody = GetComponent<Rigidbody>();
+        _material = GetComponent<Renderer>().material;
     }
 
-    private void OnCollisionEnter(Collision collision)
+    public void SetColor(Color color)
     {
-        if (collision.collider.GetComponent<Plane>() != null && _isFirstCollision)
-        {
-            _color.SetColor(UnityEngine.Random.ColorHSV());
-            _isFirstCollision = false;
-            Collided?.Invoke(this);
-        }
-    }
-
-    public void Init()
-    {
-        _isFirstCollision = true;
-        _color.SetColor(Color.blue);
-        _rigidbody.velocity = Vector3.zero;
-        _rigidbody.angularVelocity = Vector3.zero;
-        transform.rotation = Quaternion.identity;
+        _material.color = color;
     }
 }
